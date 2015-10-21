cbfbce2593b3f5a69bdfae84c05fad0947602aca
Dhanush123 <dhanush.patel@ymail.com>
2015-10-21 16:11:31 -0700

    yo
    commit 1

---
+++ SomeOperationCalculator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class SomeOperationCalculator {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		//insert type of operation here
+		System.out.println("An ___ calculator by Dhanush Patel.");
+		
+		System.out.println("Enter the first number: ");
+		double input1 = scanner.nextDouble();
+		
+		System.out.println("Enter the second number: ");
+		double input2 = scanner.nextDouble();
+		
+		double result = ; //insert operation here
+		
+		System.out.println("Result = " + result);
+	}
+}
