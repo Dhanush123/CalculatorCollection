6c59ab2c08d6548e346326200b755a55aa483f65
Dhanush123 <dhanush.patel@ymail.com>
2015-10-21 16:15:35 -0700

    commit 2
    title change

--- AddingCalculator.java
+++ AddingCalculator.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class SomeOperationCalculator {
+public class AddingCalculator {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
