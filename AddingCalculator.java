a17dd1d253aa52bf597c603a9bdfa5c837c846c5
Dhanush123 <dhanush.patel@ymail.com>
2015-10-21 17:53:28 -0700

    commit 5
    commit 5

--- AddingCalculator.java
+++ AddingCalculator.java
@@ -18,7 +18,10 @@ public class AddingCalculator {
 		System.out.println("Enter the fourth number: ");
 		double input4 = scanner.nextDouble();
 		
-		double result = input1 + input2 + input3 + input4; //insert operation here
+		System.out.println("Enter the fifth number: ");
+		double input5 = scanner.nextDouble();
+
+		double result = input1 + input2 + input3 + input4 + input5; //insert operation here
 		
 		System.out.println("Result = " + result);
 	}
